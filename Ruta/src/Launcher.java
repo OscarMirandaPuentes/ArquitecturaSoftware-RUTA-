5c2fcdc36ee524877a23eda4b5e0e0020f6d188e
ChiaraUsa <chiara.valenzuela01@correo.usa.edu.co>
2024-03-11 19:31:58 -0500

    Anotación y eliminacipon de clases extra

--- Ruta/src/Launcher.java
+++ Ruta/src/Launcher.java
@@ -8,6 +8,11 @@ public class Launcher {
         Servidor server = new Servidor("localhost", 2);
         if (args.length > 0) {
             server.start(Integer.parseInt(args[0]));
+            /* 
+            // PANTALLA DE INICIO
+            // PANTALLA DE SELECCION DE NUMERO DE JUGADORES
+            // PANTALLA DE FINALIZACIÓN
+            */
         } else {
             server.start(2396);
         }
