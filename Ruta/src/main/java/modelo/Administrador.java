9139512b4d0e95a195c4319b1d5901ae34442748
Oscar Miranda Puentes <110064371+OscarMirandaPuentes@users.noreply.github.com>
2024-03-15 12:03:46 -0500

    Creación de la base (modelo)

---
+++ Ruta/src/main/java/modelo/Administrador.java
@@ -0,0 +1,28 @@
+package modelo;
+
+public class Administrador {
+    public Juego j;
+
+    public Administrador(){
+        this.j=new Juego();
+    }
+
+    public void iniciarJuego(Mazo mazo){
+        j.repartir(mazo);
+    }
+
+    public boolean finJuego(){
+        if (j.equipo1.obtenerPuntaje()>j.getMaxPuntuacion() || j.equipo2.obtenerPuntaje()>j.getMaxPuntuacion()) {
+            return true;
+        }
+        return false;
+    }
+
+    public Juego getJ() {
+        return j;
+    }
+
+    public void setJ(Juego j) {
+        this.j = j;
+    }
+}
---
+++ Ruta/src/main/java/modelo/Jugador.java
@@ -0,0 +1,128 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jugador {
+
+    public int id;
+    public String nombre;
+    Mazo m = Mazo.getInstance();
+    public List<Carta> mano;
+
+    public Jugador(String n){
+        this.nombre = n;
+        mano = new ArrayList<Carta>();
+    }
+
+    public void recibirCarta(Carta c){
+        mano.add(c);
+    }
+
+    public void robar(){
+
+        mano.add(m.dar());
+    }
+
+    private void descartar(int o) {
+        Descarte descarte=new Descarte();
+        descarte.recibirCarta(mano.remove(o));
+    }
+
+
+    public boolean tipoAccion(int o, Equipo e, Equipo eC, int opcionAccion) {
+        Carta cartaSeleccionada = mano.get(o);
+
+        if (opcionAccion == 0) {
+
+            System.out.println("Ha seleccionado jugar la carta: " + cartaSeleccionada.tipo);
+            
+            if (cartaSeleccionada.getClass() == Peligro.class) {
+                e.anotador.aumentarPuntuacion(cartaSeleccionada);
+                boolean posible = e.atacar(cartaSeleccionada, eC);
+                System.out.println(posible);
+                if (posible){
+                    descartar(o);
+                }
+                return posible;
+            }
+            else if (cartaSeleccionada.getClass() == Defensa.class) {
+                e.anotador.aumentarPuntuacion(cartaSeleccionada);
+                boolean posible = cartaSeleccionada.accion(e);
+                System.out.println(posible);
+                if (posible){
+                    descartar(o);}
+                return posible;
+            } else {
+                boolean posible = cartaSeleccionada.accion(e);
+                System.out.println(posible);
+                if (posible) {
+                    e.anotador.aumentarPuntuacion(cartaSeleccionada);
+                    System.out.println(e.obtenerPuntaje());
+                    descartar(o);
+                }
+                return posible;
+            }
+
+        } else if (opcionAccion == 1) {
+
+            descartar(o);
+            System.out.println("Ha seleccionado descartar la carta.");
+            return true;
+
+        } else {
+
+            System.out.println("Opción inválida. Por favor, seleccione una opción válida.");
+            return false;
+
+        }
+    }
+
+    public boolean tipoAccionAsync(Carta cartaSeleccionada, Equipo e, Equipo eC) {
+        if (cartaSeleccionada != null){
+            System.out.println("Ha seleccionado jugar la carta: " + cartaSeleccionada.tipo);
+
+            if (cartaSeleccionada.getClass() == Peligro.class) {
+                e.anotador.aumentarPuntuacion(cartaSeleccionada);
+                boolean posible = e.atacar(cartaSeleccionada, eC);
+                return posible;
+            }
+            else if (cartaSeleccionada.getClass() == Defensa.class) {
+                e.anotador.aumentarPuntuacion(cartaSeleccionada);
+                boolean posible = cartaSeleccionada.accion(e);
+                return posible;
+            } else {
+                boolean posible = cartaSeleccionada.accion(e);
+                return posible;
+            }
+        }
+        else {
+            return true;
+        }
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Carta> getMano() {
+        return mano;
+    }
+
+    public void setMano(List<Carta> mano) {
+        this.mano = mano;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+
