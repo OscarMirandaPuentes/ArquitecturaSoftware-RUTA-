c822920c2b6b81ba606e839447b1d11a511ba39e
mitch <mitchellbermin10@gmail.com>
2024-03-18 17:53:51 -0500

    Fix

--- Ruta/ruta/target/ruta/js/MyScript.js
+++ Ruta/ruta/target/ruta/js/MyScript.js
@@ -1,25 +1,20 @@
+function saludar() {
+    let a = $("#a").val();
+    let b = $("#b").val();
+    let myData = {
+        a: a,
+        b: b
+    };
+    $.ajax({
+        url: 'hola',
+        type: 'GET',
+        data: myData,
+        success: function (r) {
+            paintAns(r);
+        }
+    });
+}
 
- function saludar(){
-	
-	let a=$("#a").val();
-	let b=$("#b").val();
-	let myData={
-		a:a,
-		b:b
-	};
-	$.ajax({
-		url:'hola',
-		type:'GET',
-		data: myData,
-		
-		success:function(r){
-			paintAns(r);
-		}
-	});
-	
- }
- 
- function paintAns(ans){
-	$("#resultado").html("<h2>El resultado es: "+r+"</h2>");
-	
- }
+function paintAns(ans) {
+    $("#resultado").html("<h2>El resultado es: " + ans + "</h2>");
+}
