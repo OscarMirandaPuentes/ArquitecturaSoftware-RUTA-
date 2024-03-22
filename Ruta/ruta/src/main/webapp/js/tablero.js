382c51b996d127444504cc4237a1967302ada406
ChiaraUsa <chiara.valenzuela01@correo.usa.edu.co>
2024-03-21 21:36:28 -0500

    Confirmación de uso de cartas

---
+++ Ruta/ruta/src/main/webapp/js/tablero.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#usar').on('click', function() {
+    alert('Has elegido usar la carta.');
+    // Acciones
+    });
+
+    $('#descartar').on('click', function() {
+        alert('Has elegido descartar la carta.');
+        //Acciones
+    });
+});
