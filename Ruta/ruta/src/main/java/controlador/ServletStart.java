c63e0354b362898baf769137bca192fa9e48e6d1
AndreaCTS <90526089+AndreaCTS@users.noreply.github.com>
2024-03-27 22:56:34 -0500

    modificar servlet

--- Ruta/ruta/src/main/java/controlador/ServletStart.java
+++ Ruta/ruta/src/main/java/controlador/ServletStart.java
@@ -35,17 +35,21 @@ import java.io.IOException;
         public void insertarJugador(int id, String nombre){
             
             if (id % 2 == 0) {
+                System.out.println("se crea jugador 1");
                 Jugador jugador = a.getJ().crearJugador(nombre);
                 a.getJ().getEquipo1().agregarJugador(jugador);
+                //System.out.println(a.getJ().getEquipo1());
             } else {
+                System.out.println("se crea jugador 2");
                 Jugador jugador = a.getJ().crearJugador(nombre);
                 a.getJ().getEquipo2().agregarJugador(jugador);
+                //System.out.println(a.getJ().getEquipo2());
             }
         }
 
 
         protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            System.out.println(request.getParameter("id"));
+            System.out.println("si es aqui"+request.getParameter("id"));
             int id = Integer.parseInt(request.getParameter("id"));
             String nombre = request.getParameter("nombre");
 
