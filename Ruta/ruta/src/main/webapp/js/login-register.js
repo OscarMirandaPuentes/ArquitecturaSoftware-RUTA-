d140c19daad7f8f9291d94bca5d4dc4f9b9afa74
ChiaraUsa <chiara.valenzuela01@correo.usa.edu.co>
2024-04-04 14:54:21 -0500

    Base de homepage

--- Ruta/ruta/src/main/webapp/js/login-register.js
+++ Ruta/ruta/src/main/webapp/js/login-register.js
@@ -2,6 +2,9 @@ var a = document.getElementById("loginBtn");
 var b = document.getElementById("registerBtn");
 var x = document.getElementById("loginc");
 var y = document.getElementById("registerc");
+const btnLogin = document.getElementById('login');
+const btnRegister = document.getElementById('register');
+
 
 function goLogin() {
     x.style.left = "4px";
@@ -21,3 +24,27 @@ function goRegister() {
     y.style.opacity = 1;
 }
 
+// Agregar manejadores de eventos a los botones
+btnLogin.addEventListener('click', function(event) {
+    event.preventDefault(); 
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    if (!email.trim() || !password.trim()) {
+        alert('Por favor, complete todos los campos.');
+        return;
+    }
+    window.location.href = 'home.html';
+});
+
+btnRegister.addEventListener('click', function(event) {
+    event.preventDefault(); 
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    if (!name.trim() || !email.trim() || !password.trim()) {
+        alert('Por favor, complete todos los campos.');
+        return;
+    }
+    //Agregar encriptación de contraseña
+    goLogin();
+});
