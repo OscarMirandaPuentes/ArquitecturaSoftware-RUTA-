b3ba1d93caa493a085de4dde14d1b4cf9a70619d
ChiaraUsa <chiara.valenzuela01@correo.usa.edu.co>
2024-04-03 22:13:54 -0500

    Inicio de Sesión y registro

---
+++ Ruta/ruta/src/main/webapp/js/singin.js
@@ -0,0 +1,21 @@
+var a = document.getElementById("loginBtn");
+var b = document.getElementById("registerBtn");
+var x = document.getElementById("loginc");
+var y = document.getElementById("registerc");
+
+function login() {
+    x.style.left = "4px";
+    y.style.right = "-520px";
+    b.className += " white-btn";
+    a.className = "btn";
+    x.style.opacity = 1;
+    y.style.opacity = 0;
+}
+function register() {
+    x.style.left = "-510px";
+    y.style.right = "5px";
+    b.className = "btn";
+    a.className += " white-btn";
+    x.style.opacity = 0;
+    y.style.opacity = 1;
+}
