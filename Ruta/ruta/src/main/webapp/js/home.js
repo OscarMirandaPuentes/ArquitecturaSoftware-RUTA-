1630efed22f6a15992a976552778bee92455e345
mitch <mitchellbermin10@gmail.com>
2024-04-18 22:20:57 -0500

    Se implementa spring

--- Ruta/ruta/src/main/webapp/js/home.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('crear').addEventListener('click', function () {
-        window.location.href = 'newGame.html';
-    });
-
-    document.getElementById('unirse').addEventListener('click', function () {
-        window.location.href = 'games.html'; 
-    });
-});
